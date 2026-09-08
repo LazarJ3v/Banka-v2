0da669156a0ced107a45b448aa7eda75e0a31d9a
Aleksa <jevticaleksa553@gmail.com>
2026-09-08 16:49:14 +0200

    feat: Add entity and mapping for pravno lice

---
+++ 2. deo/Banka/Banka/Entiteti/PravnoLice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka.Entiteti
+{
+    public class PravnoLice
+    {
+        public virtual int Id { get; set; }
+        public virtual string NazivFirme { get; set; }
+        public virtual string Pib { get; set; }
+        public virtual string Adresa { get; set; }
+        public virtual string Grad { get; set; }
+        public virtual string Telefon { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Status { get; set; }
+        public virtual string Komentar { get; set; }
+
+        public PravnoLice()
+        {
+
+        }
+    }
+}
---
+++ 2. deo/Banka/Banka/Mapiranja/PravnoLiceMapiranja.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Banka.Entiteti;
+
+namespace Banka.Mapiranja
+{
+    class PravnoLiceMapiranja : ClassMap<PravnoLice>
+    {
+        public PravnoLiceMapiranja()
+        {
+            Table("PRAVNO_LICE");
+            Id(x => x.Id).Column("ID").GeneratedBy.TriggerIdentity();
+
+            Map(x => x.NazivFirme).Column("NAZIVFIRME");
+            Map(x => x.Pib).Column("PIB");
+            Map(x => x.Adresa).Column("ADRESA");
+            Map(x => x.Grad).Column("GRAD");
+            Map(x => x.Telefon).Column("TELEFON");
+            Map(x => x.Email).Column("EMAIL");
+            Map(x => x.Status).Column("STATUS");
+            Map(x => x.Komentar).Column("KOMENTAR");
+        }
+    }
+}
